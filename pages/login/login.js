36f1f958d4222e63f7596779eca57b967697e762
Celestia Milara <chenmeng4312@qq.com>
2025-11-23 16:51:25 +0800

    delete remember password and third-party login

--- pages/login/login.js
+++ pages/login/login.js
@@ -8,8 +8,7 @@ const loginRateLimiter = require('../../utils/loginRateLimiter.js')
 Page({
   data: {
     username: '',
-    password: '',
-    remember: false
+    password: ''
   },
   onLoad() {
     wx.setNavigationBarTitle({
@@ -26,11 +25,6 @@ Page({
   inputPassword(e) {
     this.setData({
       password: e.detail.value
-    })
-  },
-  switchRemember() {
-    this.setData({
-      remember: !this.data.remember
     })
   },
   async login() {
@@ -94,89 +88,6 @@ Page({
       errorHandler.handleError(err, '登录失败，请重试')
       loginRateLimiter.recordLoginAttempt(username, false)
     }
-  },
-  // 处理微信登录
-  async handleWechatLogin() {
-    try {
-      // 获取用户信息
-      const { result: { openid } } = await wx.cloud.callFunction({
-        name: 'login'
-      })
-
-      if (!openid) {
-        throw new Error('获取openid失败')
-      }
-
-      // 不保存 openid 到本地存储，改用令牌系统
-      // wx.setStorageSync('openid', openid)
-
-      // 获取用户信息
-      const { userInfo } = await wx.getUserProfile({
-        desc: '用于完善用户资料'
-      })
-
-      // 查询用户是否已存在
-      const userRes = await db.collection('users').where({
-        _openid: openid
-      }).get()
-
-      let userData = {
-        nickName: userInfo.nickName,
-        avatarUrl: userInfo.avatarUrl,
-        gender: userInfo.gender,
-        phoneNumber: '',
-        email: '',
-        updateTime: db.serverDate()
-      }
-
-      if (userRes.data.length === 0) {
-        // 新用户，创建记录
-        userData.createTime = db.serverDate()
-        const addRes = await db.collection('users').add({
-          data: userData
-        })
-        userData._id = addRes._id
-      } else {
-        // 更新用户信息
-        await db.collection('users').where({
-          _openid: openid
-        }).update({
-          data: userData
-        })
-        userData = { ...userRes.data[0], ...userData }
-      }
-
-      // 使用新的认证系统保存登录状态和令牌
-      auth.saveLoginToken(userData, 'wechat')
-
-      errorHandler.showSuccess('登录成功')
-
-      // 跳转到首页
-      setTimeout(() => {
-        wx.switchTab({
-          url: '/pages/index/index'
-        })
-      }, 500)
-    } catch (err) {
-      wx.hideLoading()
-      errorHandler.handleError(err, '微信登录失败，请重试')
-    }
-  },
-
-  // 处理微博登录
-  handleWeiboLogin() {
-    wx.showToast({
-      title: '暂不支持微博登录',
-      icon: 'none'
-    })
-  },
-
-  // 处理QQ登录
-  handleQQLogin() {
-    wx.showToast({
-      title: '暂不支持QQ登录',
-      icon: 'none'
-    })
   },
 
   // 跳转到注册页面
